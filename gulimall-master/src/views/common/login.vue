6a3f1bcdecf75468826d24acbc9e1f3ff97a1245
qian520 <794308528@qq.com>
2020-11-26 14:08:11 +0800

    20201126

--- gulimall-master/src/views/common/login.vue
+++ gulimall-master/src/views/common/login.vue
@@ -5,7 +5,7 @@
 
       <div class="title-container">
 
-        <h3 class="title"> <img v-if="logo" :src="logo" class="sidebar-logo">&nbsp;&nbsp;&nbsp;&nbsp;钱多多商城</h3>
+        <h3 class="title"> <img v-if="logo" :src="logo" class="sidebar-logo">&nbsp;&nbsp;&nbsp;&nbsp;谷粒商城</h3>
       </div>
 
       <el-form-item prop="username">
